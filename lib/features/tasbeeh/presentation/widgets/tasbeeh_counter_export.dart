572cb2e6435895334c13e67595c41d19b62b670c
djihane Ghilani <djihaneghilani@gmail.com>
2023-11-20 22:48:47 +0100

    refactored tasbeeh counter export file

--- lib/features/tasbeeh/presentation/widgets/tasbeeh_counter_export.dart
+++ lib/features/tasbeeh/presentation/widgets/tasbeeh_counter_export.dart
@@ -2,4 +2,5 @@ export 'base_painter.dart';
 export 'double_circular_slider.dart';
 export 'circular_slider_paint.dart';
 export 'slider_painter.dart';
+export 'tasbeeh_counter.dart';
 export 'utils.dart';
